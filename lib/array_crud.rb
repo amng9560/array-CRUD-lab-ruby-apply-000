5ede3f0aaccabbd13fb76bf2c0eca7afdad0ad79
Amy Nguyen <amy.nguyen730@gmail.com>
2019-08-15 03:18:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,34 +7,34 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  famous_cats = ["lil' bub", "grumpy cat", "Maru"]
-  famous_cats << "arrays!"
+  cats = ["cat", "small cat", "Haku"]
+  cats << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  famous_cats = ["lil' bub", "grumpy cat", "Maru"]
-  famous_cats.unshift("wow")
+  cats = ["lil' bub", "grumpy cat", "Maru"]
+  cats.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  arrays! = famous_cats.pop
+  arrays! = cats.pop
 end
 
 def remove_element_from_start_of_array(array)
-  wow = famous_cats.shift
+  wow = cats.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  famous_cats =  ["Cheshire Cat", "Puss in Boots", "Garfield"]
+  famous_cats =  ["Neko bus", "Puss in Boots", "Cats"]
   famous_cats[0]
 end
 
 def retrieve_first_element_from_array(array)
-  famous_cats =  ["Cheshire Cat", "Puss in Boots", "Garfield"]
+  famous_cats =  ["Neko bus", "Puss in Boots", "Cats"]
   famous_cats[1]
 end
 
 def retrieve_last_element_from_array(array)
-  famous_cats =  ["Cheshire Cat", "Puss in Boots", "Garfield"]
+  famous_cats =  ["Neko bus", "Puss in Boots", "Cats"]
   famous_cats[2]
 end
